69ffd39255a8463ad5f919e338f43b093a11e1aa
GeebToo <jea.guibert@gmail.com>
2018-03-12 18:59:31 +0100

    chore: fix security vulnerability in one of your dependencies and update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module',
+    parser: 'babel-eslint'
   },
   globals: {
     localStorage: true,
     JSON: true,
     XMLHttpRequest: true
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
+  env: {
+    browser: true
+  },
+  extends: ['plugin:vue/recommended'],
+  plugins: ['vue'],
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
     'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'vue/max-attributes-per-line': 'off'
   }
 }
